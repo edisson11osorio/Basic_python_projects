69dd6aa51454c5ee6d1e9f796261c043a1868565
edisson11osorio <osorioedisson08@gmail.com>
2022-03-03 14:26:22 -0500

    historias_locas

---
+++ historias_locas/historias_locas.py
@@ -0,0 +1,10 @@
+ #!/usr/bin/python3
+
+adj = input ("adjetivo: ")
+verbo_1 = input ("verbo: ")
+verbo_2 = input ("verbo: ")
+sustantivo_plural = input ("sustantivo (plural): ")
+
+madlib = f"programar es tan {adj}! siempre me emociona porque me encanta {verbo_1} prolemas. !Aprende a {verbo_2} con FreeCodeCapm y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
